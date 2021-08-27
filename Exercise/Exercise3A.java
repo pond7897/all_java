bf650c5a6136ac10991942a146d6f9b35de8930c
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-08-27 15:11:00 +0700

    Create Exercise3A.java

---
+++ Exercise/Exercise3A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Exercise3A {
+  public static void main(String[] args) {
+   	double f,c1,c2,c3,c4;
+    f=82;
+    c1 = 5/9*(f-32); c2 = 5*(f-32)/9;
+    c3 = (f-32)*5/9; c4 = (f-32)/9*5;
+    System.out.println("F1 = "+c1);
+    System.out.println("F2 = "+c2);
+    System.out.println("F3 = "+c3);
+    System.out.println("F4 = "+c4);
+  
+    
+  }
+}
