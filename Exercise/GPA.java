55fecd5e1bb7666c00440ddf9cb81a2ece8693f3
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-08-27 15:29:20 +0700

    แก้ไขให้ code กระทัดรัดมากขึ้นและสั้นลง

--- Exercise/GPA.java
+++ Exercise/GPA.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 class GPA
 {
 	public static void main(String[] args){
-	Scanner thai = new Scanner(System.in);
+	double sub1,sub2,sub3,sub4,sum;
+		Scanner subject = new Scanner(System.in);
+		System.out.print("input grade thai"); sub1 = subject.nextDouble();
+		System.out.print("input grade math"); sub2 = subject.nextDouble();
+		System.out.print("input grade english"); sub3 = subject.nextDouble();
+		System.out.print("input grade social"); sub4 = subject.nextDouble();
+	sum = (sub1+sub2+sub3+sub4)/4;
+		Symtem.out.println("GPA is " +sum);
+		
+		
+	/*Scanner thai = new Scanner(System.in);
 	System.out.print("input grade thai:" );
 	double sub1 = thai.nextDouble();
 	Scanner math = new Scanner(System.in);
@@ -15,7 +25,7 @@ class GPA
 	System.out.print("input grade social:" );
 	double sub4 = social.nextDouble();
 	double sum = (sub1+sub2+sub3+sub4)/4;
-	System.out.println("GPA = " +sum);
+	System.out.println("GPA = " +sum);*/
 	
 	
 	}
