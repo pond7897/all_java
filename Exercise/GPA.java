63e2611ddb09ec1549efc19032acf1c41bb144ff
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-08-27 15:29:52 +0700

    Update GPA.java

--- Exercise/GPA.java
+++ Exercise/GPA.java
@@ -13,19 +13,19 @@ class GPA
 		
 		
 	/*Scanner thai = new Scanner(System.in);
-	System.out.print("input grade thai:" );
-	double sub1 = thai.nextDouble();
-	Scanner math = new Scanner(System.in);
-	System.out.print("input grade math:" );
-	double sub2 = math.nextDouble();
-	Scanner eng = new Scanner(System.in);
-	System.out.print("input grade eng:" );
-	double sub3 = eng.nextDouble();
-	Scanner social = new Scanner(System.in);
-	System.out.print("input grade social:" );
-	double sub4 = social.nextDouble();
-	double sum = (sub1+sub2+sub3+sub4)/4;
-	System.out.println("GPA = " +sum);*/
+	  System.out.print("input grade thai:" );
+	  double sub1 = thai.nextDouble();
+	  Scanner math = new Scanner(System.in);
+	  System.out.print("input grade math:" );
+	  double sub2 = math.nextDouble();
+	  Scanner eng = new Scanner(System.in);
+	  System.out.print("input grade eng:" );
+	  double sub3 = eng.nextDouble();
+	  Scanner social = new Scanner(System.in);
+	  System.out.print("input grade social:" );
+	  double sub4 = social.nextDouble();
+	  double sum = (sub1+sub2+sub3+sub4)/4;
+	  System.out.println("GPA = " +sum);*/
 	
 	
 	}
