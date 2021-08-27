b6305f38a380234cd9d1ab5e2cdd6d532ac8a1e0
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-08-27 15:32:04 +0700

    Create Exercise3B.java

---
+++ Exercise/Exercise3B.java
@@ -0,0 +1,14 @@
+//หาค่าสุดท้ายว่าได้เท่าไหร่ แล้วถ้าคิดมือจะได้เท่าไหร่
+class Exercise3B {
+  public static void main(String[] args) {
+    double x=1;
+    x += 3;
+    x *= 10;
+    x -=10;
+    x /=5;
+    x %=5;
+    System.out.println("Answer is :" +x);
+    
+    
+  }
+}
