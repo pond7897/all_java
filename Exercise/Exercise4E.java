abc68776d3ee6d7eb0867a693075a7fadeffc68d
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-08-31 01:10:01 +0700

    Update Exercise4E.java

--- Exercise/Exercise4E.java
+++ Exercise/Exercise4E.java
@@ -2,12 +2,14 @@ import java.lang.Math;
 import java.util.Scanner;
 class Exercise4E {
   public static void main(String[] args) {
-    double x1,y1;
-	double x2,y2;
+    	double x1,y1;
+	double x2,y2; //อยากประกาศแยกเพราะว่ามันจะไม่ลายตา
+//1	  
     Scanner kbd = new Scanner(System.in);
     System.out.print("input x1 : "); x1 = kbd.nextDouble();
     y1 = Math.log10(3.0*(x1*x1))+(2.0/3.0)*(x1*x1*x1)+Math.sin(2*x1);
     System.out.println("value of function :" +y1);
+//2	  
     System.out.print("input x2 : "); x2 = kbd.nextDouble();
     y2 = Math.log10(3.0*(x2*x2))+(2.0/3.0)*(x2*x2*x2)+Math.sin(2*x2);
     System.out.println("value of function :" +y2);
