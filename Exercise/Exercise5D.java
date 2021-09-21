dd9e4f45c7ec6c55a8349d414332ed2daa772ea5
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-09-21 15:13:24 +0700

    Add files via upload

---
+++ Exercise/Exercise5D.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Exercise5D
+{
+	private static double getDouble(String msg){
+	Scanner kbd = new Scanner(System.in);
+	System.out.print(msg); double num = kbd.nextDouble();
+	return num;
+	}
+	public static double funcF(double x,double y){
+		double f,num_up,num_down;
+		num_up = Math.pow(x,2)+Math.pow(y,5);
+		num_down = x+y+1.0;
+		f = num_up/num_down;
+		return f;
+	}
+	public static void main(String[] args){
+		double a,b,z;
+		a = getDouble("Enter -A value :");
+		b = getDouble("Enter -B value :");
+		z = funcF(a,b);
+		System.out.println("F value is : "+z);
+	}
+}
+//Titpong Keawkhum 64050407
---
+++ Exercise/Exercise6D.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Exercise6D
+{
+	public static double getDouble(String msg){
+		Scanner kbd = new Scanner(System.in);
+		System.out.print(msg); double num = kbd.nextDouble();
+	return num;
+	}
+	public static void quadSlove(double a,double b,double c){
+		double r,x1,x2;
+		if (a != 0){
+			r = Math.pow(b,2)-(4.0*a*c);
+		if (r >= 0){
+				x1 = (-b + Math.sqrt(r))/2.0*a;
+				x2 = (-b - Math.sqrt(r))/2.0*a;
+				System.out.println("root 1 :" +x1);
+				System.out.println("root 2 :" +x2);
+		}
+		else System.out.println("no real root");
+	}
+	else System.out.println("The equation does not quadratic");
+}
+	public static void main(String[] args){
+		double a,b,c,f;
+		a = getDouble("Enter a : ");
+		b = getDouble("Enter b : ");
+		c = getDouble("Enter c : ");
+		quadSlove(a,b,c);
+		
+	}
+}
+//Thanida Sangsri 64050065
