663f5dfe6e7266c951b9d766ed319771726d2151
pond7897 <80212418+pond7897@users.noreply.github.com>
2021-12-03 20:16:41 +0700

    Create check_Int_or_Float.java

---
+++ so_so/check_Int_or_Float.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class check_Int_of_Float {
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);  int number = kbd.nextInt();
+        float sqrt = (float) Math.sqrt(number);
+        kbd.close();
+        System.out.println("sqrt of " + number + " is " + sqrt);
+        if (sqrt == Math.round(sqrt)) {
+            System.out.println("Integer");
+        } else {
+            System.out.println("Float");
+        }
+    }
+}
+/* Tips: Run for check input format */
