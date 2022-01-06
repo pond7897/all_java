12d4556a747db906c39767df24255169b5913534
pond7897 <80212418+pond7897@users.noreply.github.com>
2022-01-06 13:42:10 +0700

    Update binarySearch.java

--- so_so/binarySearch.java
+++ so_so/binarySearch.java
@@ -1,4 +1,26 @@
-package lab3;
+//แปลงจาก python เป็น java
+/* 
+def binary_search(lst, v):
+    i,j = 0, len(lst)
+    while i < j:
+        mid = (i+j)//2
+        if v < lst[mid]:
+            j = mid
+        elif v > lst[mid]:
+            i = mid+1
+        else:
+            return mid
+    return None
+
+def main():
+    lst = [1,2,4,5,7,8,11,13]
+    print(binary_search(lst, 5))
+    print(binary_search(lst, 13))
+    print(binary_search(lst, 10))
+
+if __name__ == '__main__':
+    main()
+*/
 public class binarySearch {
     //Binary search
    public static int BinarySearch(int[] lst, int v) {
