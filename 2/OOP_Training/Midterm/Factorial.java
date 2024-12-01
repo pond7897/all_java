7649a55eca0b8d1dd33010d65e0bfef7c9db8297
Titipong Keawkhum <80212418+pond7897@users.noreply.github.com>
2024-12-01 14:53:02 +0700

    Update .gitignore and clean up Factorial.java

--- 2/OOP_Training/Midterm/Factorial.java
+++ 2/OOP_Training/Midterm/Factorial.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter n : ");
 		
